a84bb53c9ce3d1aedeb0ee374fb34cf039458aac
1234Taschenlampe <jan.felix.eckert@gmx.de>
2026-06-09 20:52:08 +0200

    Update prank sequence with desktop screenshot background and game.html with gooey slime splitting and boss push-back physics

--- prank.ps1
+++ prank.ps1
@@ -10,13 +10,28 @@ if ($oldWallpaper -and $oldWallpaper -notlike "*wallpaper.png") {
     $oldWallpaper | Out-File "$targetDir\original_wallpaper.txt" -Force
 }
 
-# 1. Set System Volume to 100%
+# 1. Take a screenshot of the desktop to use as the background in game.html
+try {
+    Add-Type -AssemblyName System.Windows.Forms -ErrorAction Stop
+    Add-Type -AssemblyName System.Drawing -ErrorAction Stop
+    $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+    $bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height
+    $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphics.CopyFromScreen($screen.Location, [System.Drawing.Point]::Empty, $screen.Size)
+    $bitmap.Save("$targetDir\desktop.png", [System.Drawing.Imaging.ImageFormat]::Png)
+    $graphics.Dispose()
+    $bitmap.Dispose()
+} catch {
+    Write-Host "Screenshot failed: $_"
+}
+
+# 2. Set System Volume to 100%
 $wshShell = New-Object -ComObject WScript.Shell
 for ($i = 0; $i -lt 50; $i++) {
     $wshShell.SendKeys([char]175) # Volume Up
 }
 
-# 2. Change Desktop Wallpaper to the Prank Cat
+# 3. Change Desktop Wallpaper to the Prank Cat
 $code = @'
 using System;
 using System.Runtime.InteropServices;
@@ -31,5 +46,5 @@ if (Test-Path $wallpaperPath) {
     [Wallpaper]::SystemParametersInfo(20, 0, $wallpaperPath, 3) | Out-Null
 }
 
-# 3. Open the lock screen mini-game
+# 4. Open the lock screen mini-game (game.html uses desktop.png as background)
 Start-Process "http://localhost:8088/game.html"
