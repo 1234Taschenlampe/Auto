10f4f6313e59a29c25a39ee1a57b85ab7aaab128
1234Taschenlampe <135506164+1234Taschenlampe@users.noreply.github.com>
2026-09-24 12:21:36 +0200

    Track manual Pixel Mode state

--- wifiquiet/project/app/src/main/java/de/jfe/wifiquiet/Config.java
+++ wifiquiet/project/app/src/main/java/de/jfe/wifiquiet/Config.java
@@ -4,8 +4,8 @@ import android.content.SharedPreferences;
 final class Config {
     static final String PREFS="wifiquiet";
     static final String ENGINE_ENABLED="engine_enabled", SERVICE_RUNNING="service_running",
-            ENGINE_ACTIVE_IDS="engine_active_ids", LAST_STATUS="last_status",
-            BASELINE_VALID="baseline_valid", BASELINE_RESTORE="baseline_restore",
+            ENGINE_ACTIVE_IDS="engine_active_ids", MANUAL_ACTIVE_IDS="manual_active_ids", SNOOZED_IDS="snoozed_ids",
+            LAST_STATUS="last_status", BASELINE_VALID="baseline_valid", BASELINE_RESTORE="baseline_restore",
             ORIGINAL_MEDIA="original_media", ORIGINAL_RING="original_ring", ORIGINAL_NOTIFICATION="original_notification",
             ORIGINAL_BRIGHTNESS="original_brightness", ORIGINAL_ROTATION="original_rotation", ORIGINAL_TIMEOUT="original_timeout";
     private Config(){}
